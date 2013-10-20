11638f4a29266e350df0c3eb48b3d680cf84c856
alexbirukov <lord_maloy@mail.ru>
2013-10-20 21:52:48 +0600

    Update to version 0.2
    - bug fix;
    - create settings;
    - auto fill senders field;

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ SMS Pilot Sender
 ================
 
 SMS Pilot Sender plug-in for Symphony CMS
+
+- Version: 0.1
+- Author: Alexander Biryukov
+- Build Date: 19 October 2013
+- Requirements: Symphony 2.3.3
+
+## Installation
+
+1. Upload the 'sms_pilot_sender' folder in this archive to your Symphony 'extensions' folder.
+2. Enable it by selecting the "SMS Pilot Sender" Extension, choose Enable from the with-selected menu, then click Apply.
+3. You can now edit the SMS Pilot Sender setting in the configuration section from within the Symphony administration interface.
+
+## Change Log
+
+Version 0.1 - 19 October 2013
+
+- Create plug-in
+
+
--- extension.driver.php
+++ extension.driver.php
@@ -5,7 +5,7 @@
 		public function about() {
 			return array(
 				'name' => 'SMS Pilot Sender',
-				'version' => '0.1',
+				'version' => '0.2',
 				'release-date' => '2013-10-19',
 				'author' => array(
 					'name' => 'Александр Бирюков',
@@ -16,6 +16,9 @@
 			);
 		}
 
+		/*
+		* Подписываем делегаты
+		*/
 		public function getSubscribedDelegates() {
 			return array(
 				array(
@@ -49,17 +52,16 @@
 		
 		/**
 		 * Add site preferences
+		 * Добавляем в раздел настроек новые поля
 		 */
 		public function __addPreferences($context) {
 
-			// Get settings
-			$selection = Symphony::Configuration()->get('smspilot');
-			if(empty($selection)) $selection = array();
-
 			// Create Group
+			// Создаём группу
 			$group = new XMLElement('fieldset', '<legend>' . __('SMSPilot Sender') . '</legend>', array('class' => 'settings'));
 			
 			// Add API Key field
+			// Добавляем поле для ввода API ключа
 			$label_api = Widget::Label(__('API Key'),
                 Widget::Input(
                     'settings[smspilot][api_key]',
@@ -69,59 +71,106 @@
 			$help = new XMLElement('p', __('To get the API key you need visit <a href="http://www.smspilot.ru/?r=6708">SMS Pilot</a>.'), array('class' => 'help'));
 			$group->appendChild($help);
 			
-			// Add FROM field
-			$label_api = Widget::Label(__('From'),
-                Widget::Input(
-                    'settings[smspilot][from]',
-                    Symphony::Configuration()->get('from', 'smspilot')
-                ));
-			$group->appendChild($label_api);
-			$help = new XMLElement('p', __('Optional. Set sender name in the settings <a href="http://www.smspilot.ru/?r=6708">SMS Pilot</a> and fill this field.'), array('class' => 'help'));
-			$group->appendChild($help);
+			// Get settings
+			// Если API ключ введён, получаем его из настроет и через API сервиса получаем список имён отправителя
+			$apikey = Symphony::Configuration()->get('api_key', 'smspilot');
 			
-			// Add phones field
-			$label_phones = Widget::Label(__('Phones'),
-                Widget::Input(
-                    'settings[smspilot][phones]',
-                    Symphony::Configuration()->get('phones', 'smspilot')
-                ));
-			$group->appendChild($label_phones);
-			$help = new XMLElement('p', __('Enter your phone number in the format 7XXXXXXXXXX or 8XXXXXXXXXX. The phone numbers are separated by commas.'), array('class' => 'help'));
-			$group->appendChild($help);
+			// API 2
+			// Делаем запрос к сервису
+			$send = array(
+				'apikey' => $apikey,
+				'info' => true
+			);
+			// Результат
+			$result = file_get_contents("http://smspilot.ru/api2.php?r=6708", false, stream_context_create(array(
+				"http" => array(
+					"method" => "POST",
+					"header" => "Content-Type: application/json\r\n",
+					"content" => json_encode( $send ),
+				),
+			)));
 			
-			// Add message field
-			$message = Widget::Label(__('Message text'),
-                Widget::Input(
-                    'settings[smspilot][message]',
-                    Symphony::Configuration()->get('message', 'smspilot')
-                ));
-			$group->appendChild($message);
-			$help = new XMLElement('p', __('Enter your message text.'), array('class' => 'help'));
-			$group->appendChild($help);
+			// Get response
+			// Разбираем ответ от сервиса
+			$response = json_decode($result);
+			
+			// If API key valid
+			// Если API ключ корректен, отображаем дополнительные поля настроек
+			if(isset($response->{'info'}->{'id'})) {
+				
+				// Разбираем список имён отправителя
+				$tmp = $response->{'info'}->{'senders'};
+				$senders = explode(",", $tmp);
+				
+				// Создаём массив параметров
+				$options = array();
+				$from = Symphony::Configuration()->get('from', 'smspilot');
+				foreach($senders as $sender) {
+					$options[] = array($sender, (($from == $sender) ? true : false), $sender);
+				}
+				
+				// Add FROM field
+				// Добавляем поле с именем отправителя
+				$label_from = Widget::Label(__('From'),
+					Widget::Select('settings[smspilot][from]', $options, array('class' => 'picker'))	
+				);
+				
+				$group->appendChild($label_from);
+				$help = new XMLElement('p', __('Optional. Set sender name in the settings <a href="http://www.smspilot.ru/?r=6708">SMS Pilot</a> and fill this field.'), array('class' => 'help'));
+				$group->appendChild($help);
+
+				// Add phones field
+				// Добавляем поле с номерами получателей
+				$label_phones = Widget::Label(__('Phones'),
+					Widget::Input(
+						'settings[smspilot][phones]',
+						Symphony::Configuration()->get('phones', 'smspilot')
+					));
+				$group->appendChild($label_phones);
+				$help = new XMLElement('p', __('Enter your phone number in the format 7XXXXXXXXXX or 8XXXXXXXXXX. The phone numbers are separated by commas.'), array('class' => 'help'));
+				$group->appendChild($help);
+				
+				// Add message field
+				// Добавляем поле с сообщением
+				$message = Widget::Label(__('Message text'),
+					Widget::Input(
+						'settings[smspilot][message]',
+						Symphony::Configuration()->get('message', 'smspilot')
+					));
+				$group->appendChild($message);
+				$help = new XMLElement('p', __('Enter your message text.'), array('class' => 'help'));
+				$group->appendChild($help);
+			}
 			
 			$context['wrapper']->appendChild($group);
 		}
 		
 		/**
 		 * Save preferences
+		 * Перед сохранением настроек
 		 */
 		public function __savePreferences($context) {
 
 			// Remove chars
+			// Удаляем лишние символы из номеров телефонов
 			$chars = array("(", ")", "-", "+", " ");
 			$tmp = str_replace($chars, "",$context['settings']['smspilot']['phones']);
 			$context['settings']['smspilot']['phones'] = str_replace(",", ", ",$tmp);
 			
 			// Trim message text
+			// Удаляем лишние пробелы из сообщения
 			$context['settings']['smspilot']['message'] = trim($context['settings']['smspilot']['message']);
 
 		}
 		
 		/**
 		 * Append filter after save
+		 * Срабатывание фильтра
 		 */
 		public function __eventFinalSaveFilter($context){
-		
+			
+			// Errors
+			// Список ошибок
 			$sms_errors = array();
 			$sms_errors['10'] = __('INPUT data is required');
 			$sms_errors['11'] = __('Unknown INPUT format');
@@ -158,9 +207,12 @@
 			$sms_errors['401'] = __('Invalid login details');
 			
 			// Send SMS
+			// Отправка СМС
 			
 			// Get settings
+			// Получаем настроки
 			$apikey = Symphony::Configuration()->get('api_key', 'smspilot');
+			// API для отладки
 			//$apikey = 'XXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZXXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZ';
 			$phones = Symphony::Configuration()->get('phones', 'smspilot');
 			$message = Symphony::Configuration()->get('message', 'smspilot');
@@ -193,6 +245,7 @@
 					$sms_errors[$response->{'error'}->{'code'}]
 				);
 				
+				// Делаем запись в лог системы
 				Symphony::Log()->pushToLog(__('SMSPilot sender: ') . $sms_errors[$response->{'error'}->{'code'}], 1, true);
 			}	
 			else
@@ -204,6 +257,7 @@
 						$sms_errors[$response->{'send'}[0]->{'error'}]
 					);
 					
+					// Делаем запись в лог системы
 					Symphony::Log()->pushToLog(__('SMSPilot sender: ') . $sms_errors[$response->{'send'}[0]->{'error'}], 1, true);
 				}
 				else
@@ -219,6 +273,7 @@
 		
 		/**
 		 * Append Filter
+		 * Добавляем фильтр в список фильтров событий
 		 */
 		public function __appendEventFilter(array $context) {
 			$context['options'][] = array(
@@ -227,5 +282,16 @@
 				__('SMS Pilot: Send SMS')
 			);
 		}
+		
+		/**
+		 * Uninstall function
+		 * Функция деинсталяции
+		 */
+		public function uninstall() {
 
+			// Remove strings from configuration
+			// Удаляем настройки из конфигурации
+			Symphony::Configuration()->remove('smspilot');
+			Symphony::Configuration()->write();
+		}
 	}
